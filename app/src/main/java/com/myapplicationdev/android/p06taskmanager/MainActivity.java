6fb0064d2299f00190bcf2a8d96be587c2658a3d
Fadillah98 <17010176@myrp.edu.sg>
2019-05-28 12:11:31 +0800

    MainActivity

--- app/src/main/java/com/myapplicationdev/android/p06taskmanager/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p06taskmanager/MainActivity.java
@@ -1,13 +1,43 @@
 package com.myapplicationdev.android.p06taskmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    Button btnAdd;
+    ArrayList<String> task;
+    ArrayAdapter<String> aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = findViewById(R.id.taskListView);
+        btnAdd = findViewById(R.id.btnAdd);
+
+        task = new ArrayList<String>();
+        DBHelper db = new DBHelper(MainActivity.this);
+        task = db.getTasks();
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 ,task);
+        lv.setAdapter(aa);
+        aa.notifyDataSetChanged();
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), AddActivity.class);
+                startActivityForResult(intent, 9);
+            }
+        });
     }
 }
